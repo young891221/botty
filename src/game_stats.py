5009c5dead35234eb8c4de310eb7a015cb187209
youngjae.havi <youngjae.havi@linecorp.com>
2023-04-17 10:29:08 +0900

    add skip raw rune

--- src/game_stats.py
+++ src/game_stats.py
@@ -51,7 +51,7 @@ class GameStats:
     def log_item_keep(self, item_name: str, send_message: bool, img: np.ndarray, ocr_text: str = '', expression: str = '', item_props: dict = {}):
         filtered_substrings = [" POTION", " OF IDENTIFY", " OF TOWN PORTAL", " AMETHYST", " RUBY", " TOPAZ", " EMERALD", " SAPPHIRE", " DIAMOND"]
         filtered_matches = ["DIAMOND", "AMETHYST", "RUBY", "TOPAZ", "EMERALD", "SAPPHIRE", "ARROWS", "BOLTS",
-                            "CHIPPED SKULL", "FLAWED SKULL", "SKULL", "FLAWLESS SKULL", "PERFECT SKULL"]
+                            "CHIPPED SKULL", "FLAWED SKULL", "SKULL", "FLAWLESS SKULL", "PERFECT SKULL", "TIR RUNE", "TAL RUNE", "RAL RUNE", "NEF RUNE"]
         skip_log = any(substring in item_name for substring in filtered_substrings) or any(match == item_name.strip() for match in filtered_matches)
         if self._location is not None and not skip_log:
             Logger.debug(f"Stashed and logged: {item_name}")
